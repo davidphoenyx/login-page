8e8d30223c99c21e32c67d3ad863528445611f1e
David Phoenyx <95734705+davidphoenyx@users.noreply.github.com>
2023-01-15 21:27:11 -0300

    Instalação do campo de recuperação de senha
    Nesse commit foi instalado e configurado o campo para recuperação de senha.

--- javascript.js
+++ javascript.js
@@ -1,43 +1,69 @@
-function notification(value1, value2) {
+function notification(value) {
 
-    if (value1) {
-        var div = document.getElementById('notification')
+    var div = document.getElementById('notification')
+
+    switch (value) {
+        case 'success':
+            div.style.cssText = 'height: 50px; color: green;'
+            div.innerText = 'LOGIN SUCCESSFULLY'
+            break
+        case 'pass':
+            div.style.cssText = 'height: 50px; color: red;'
+            div.innerText = 'FAIL TO LOGIN, WRONG PASS OR E-MAIL'
+            break
+        case 'mail':
+            div.style.cssText = 'height: 50px; color: red;'
+            div.innerText = 'FAIL TO LOGIN, VERIFY YOUR E-MAIL'
+            break
+        case 'empty':
+            div.style.cssText = 'height: 50px; color: red;'
+            div.innerText = 'FAIL TO LOGIN, EMPTY DATA'
+            break
+        case 'verified':
+            div.style.cssText = 'height: 50px; color: green'
+            div.innerText = 'E-MAIL TO RECOVERY ACCOUNT WAS SEND'
+            break
+        default:
+            div.style.cssText = 'height: 50px; color: red;'
+            div.innerText = 'FAIL TO LOGIN'
+
+    }
+
+    
+    /*if (value1) {
         div.style.cssText = 'height: 50px; color: green;'
         div.innerText = 'LOGIN SUCCESSFULLY'
     } else if (value2 == 'pass') {
-        var div = document.getElementById('notification')
         div.style.cssText = 'height: 50px; color: red;'
         div.innerText = 'FAIL TO LOGIN, WRONG PASS OR E-MAIL'
     } else if (value2 == 'mail') {
-        var div = document.getElementById('notification')
         div.style.cssText = 'height: 50px; color: red;'
         div.innerText = 'FAIL TO LOGIN, VERIFY YOUR E-MAIL'
     } else if (value2 == 'empty') {
-        var div = document.getElementById('notification')
         div.style.cssText = 'height: 50px; color: red;'
         div.innerText = 'FAIL TO LOGIN, EMPTY DATA'
+    } else if (value2 == 'verified') {
+        div.style.cssText = 'height: 50px; color: green'
+        div.innerText = 'E-MAIL TO RECOVERY ACCOUNT WAS SEND'
     } else {
-        var div = document.getElementById('notification')
         div.style.cssText = 'height: 50px; color: red;'
         div.innerText = 'FAIL TO LOGIN'
-    }
+    }*/
 
 }
 
 function closeNotification() {
 
     var div = document.getElementById('notification')
-    div.style.cssText = 'height: 0px;'
+    div.style.cssText = 'height: 0px; color: black'
 
 }
 
-function verify() {
-
-    var mail = document.getElementById('login-mail').value
+function verify(value) {
 
     var validRegex = /^[a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1}([a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1})*[a-zA-Z0-9]@[a-zA-Z0-9][-\.]{0,1}([a-zA-Z][-\.]{0,1})*[a-zA-Z0-9]\.[a-zA-Z0-9]{1,}([\.\-]{0,1}[a-zA-Z]){0,}[a-zA-Z0-9]{0,}$/i
 
-    if (mail.match(validRegex)) {
+    if (value.match(validRegex)) {
         return true
     } else {
         return false
@@ -52,19 +78,45 @@ function login() {
     var pass = document.getElementById('login-pass').value
 
     if (mail === '' || pass === '') {
-        notification(false, 'empty')
+        notification('empty')
         setTimeout(closeNotification, 3000)
-    } else if (verify()) {
+    } else if (verify(mail)) {
         if (mail === 'seu@email.com' && pass === 'senha') {
-            notification(true)
+            notification('success')
             setTimeout(closeNotification, 3000)
         } else {
-            notification(false, 'pass')
+            notification('pass')
             setTimeout(closeNotification, 3000)
         }
     } else {
-        notification(false, 'mail')
+        notification('mail')
         setTimeout(closeNotification, 3000)
     }
+
+}
+
+function send() {
+
+    var mail = document.getElementById('recovery-mail').value
+
+    if(verify(mail)) {
+        notification('verified')
+        setTimeout(closeNotification, 3000)
+    } else {
+        notification('mail')
+        setInterval(closeNotification, 3000)
+    }
+
+}
+
+function recover() {
+    
+    var divRecover = document.getElementById('recover')
+
+    var divLogin = document.getElementById('login')
+
+    divRecover.style.cssText = 'z-index: 1;'
+
+    divLogin.style.cssText = 'display: none;'
 
 }
